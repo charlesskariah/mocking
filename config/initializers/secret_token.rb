4a6ad83e41d69c386fe19ffdbe5e54e954b4a2f7
Charles <charles.skariah@imaginea.com>
2017-12-13 21:30:35 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MockTest::Application.config.secret_token = '9aa0258cf1c52b4c1cdb241de5f18040280a0563fb46c9097877ae798991b9baffed711169a736d763794cb7a1f5f3b3e4b532f1db22bfd05a32cd22ceb66934'
